68d3cbf170b9dc0a15e758dda4b53959c83d99b1
codebicycle <codebicycle@gmail.com>
2016-04-17 17:32:32 +0300

    Add css styles for inmates new form

--- application/view/inmates/new.php
+++ application/view/inmates/new.php
@@ -1,27 +1,27 @@
 <div class="container">
   <h3>Add new inmate</h3>
-  <form action="<?php echo URL; ?>inmates/create" method="POST">
-    <label>First Name</label>
-    <input type="text" name="FirstName" value="" required autofocus />
-    <label>Last Name</label>
-    <input type="text" name="LastName" value="" required />
-    <label>CNP</label>
-    <input type="text" name="CNP" value="" required />
-    <label>Institution Id</label>
-    <input type="text" name="InstId" value="" required />
-    <label>Lawyer</label>
-    <input type="text" name="Lawyer" value="" />
-    <label>Date Of Birth</label>
-    <input type="date" name="DOB" value="" required />
-    <label>Sentence</label>
-    <input type="text" name="Sentence" value="" required />
-    <label>Crime</label>
-    <input type="text" name="Crime" value="" required />
-    <label>Incarceration Date</label>
-    <input type="date" name="IncarcerationDate" value="" required />
-    <label>Release Date</label>
-    <input type="date" name="ReleaseDate" value="" required />
-
+  <form action="<?php echo URL; ?>inmates/create" method="POST" id="inmates-new-form">
+    <label for="FirstName">First Name</label>
+    <input type="text" name="FirstName" id="FirstName" required autofocus />
+    <label for="LastName">Last Name</label>
+    <input type="text" name="LastName" id="LastName" required />
+    <label for="CNP">CNP</label>
+    <input type="text" name="CNP" id="CNP" required />
+    <label for=name="DOB">Date Of Birth</label>
+    <input type="date" name="DOB" id=name="DOB" required />
+    <label for="InstId">Institution Id</label>
+    <input type="text" name="InstId" id="InstId" required />
+    <label for="Crime">Crime</label>
+    <input type="text" name="Crime" id="Crime" required />
+    <label for="Sentence">Sentence</label>
+    <input type="text" name="Sentence" id="Sentence" required />
+    <label for="IncarcerationDate">Incarceration Date</label>
+    <input type="date" name="IncarcerationDate" id="IncarcerationDate" required />
+    <label for="ReleaseDate">Release Date</label>
+    <input type="date" name="ReleaseDate" id="ReleaseDate" required />
+    <label for="Lawyer">Lawyer</label>
+    <input type="text" name="Lawyer" id="Lawyer" />
+    <label></label>
     <input type="submit" name="Create" value="Create" />
   </form>
 </div>
