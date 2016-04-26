a79ef282da3dd8fa3bd2cb0fa50d9be6b608db0a
codebicycle <codebicycle@gmail.com>
2016-04-26 21:25:42 +0300

    Refactor helpers
    Form novalidate attribute disables html5 validation.
    Useful when testing model validations. Remove in production.

--- application/libs/helpers.php
+++ application/libs/helpers.php
@@ -1,10 +1,12 @@
 <?php
 
 function cached_value($label) {
-  if (isset($_POST[$label])) echo htmlentities($_POST[$label]);
+  if (isset($_POST[$label]))
+    echo htmlentities($_POST[$label]);
 }
 
-function validation_hint($hint) {
-  if (isset($hint)) echo '<p class="validation_error">' . $hint . '</p>';
+function validation_hint($errors, $label) {
+  $hint = $errors[$label] ?? null;
+  if ($hint)
+    echo '<p class="validation_error">' . $hint . '</p>';
 }
-
