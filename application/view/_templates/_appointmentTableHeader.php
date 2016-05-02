bd2129bf4f6d1b212dff3aad1d57dabc065e1a21
alex z <alex z>
2016-05-02 16:54:30 +0300

    fix previous commit

---
+++ application/view/_templates/_appointmentTableHeader.php
@@ -0,0 +1,15 @@
+<table>
+           <thead style="background-color: #ddd; font-weight: bold;">
+             <tr>
+               <td>Inmate</td>
+               <td>Institution</td>
+               <td>Visitor</td>
+               <td>2nd visitor</td>
+               <td>3rd visitor</td>
+               <td>Date </td>
+               <td>Time</td>
+              <?php if ($State==3){  ?>
+			  <td>Motive</td><?php }?>
+             </tr>
+           </thead>
+           <tbody>
