3c557e81b0b1c95fc2101251a33f5fe652d2a754
codebicycle <codebicycle@gmail.com>
2016-05-06 10:11:59 +0300

    Logged-in user info

--- application/view/_templates/navigation.php
+++ application/view/_templates/navigation.php
@@ -1,56 +1,66 @@
+<?php require APP . 'libs/helpers.php'; ?>
+
 <div class="navigation">
 
-<?php
-if(!isset($_SESSION)) 
-        { 
-            session_start(); 
+    <?php
+    if(!isset($_SESSION))
+    {
+        session_start();
+    }
+
+    if (isset($_SESSION['user_id']))
+    {
+        ?>
+        <a href="<?php echo URL ?>">home</a>
+        <a href="<?php echo URL . 'appointments/index' ?>">appointments</a>
+        <a href="<?php echo URL . 'visits/index' ?>">visits</a>
+        <a href="<?php echo URL . 'visitors/editaccount' ?>">profile </a>
+        <a href="<?php echo URL . 'visitors/logout' ?>">logout</a>
+
+        <?php
+    }
+    else if(isset($_SESSION['admin_id']) && $_SESSION['rank'] == 0)
+    {
+        ?>
+        <a href="<?php echo URL ?>">home</a>
+        <a href="<?php echo URL . 'inmates/index' ?>">inmates</a>
+        <a href="<?php echo URL . 'appointments/index' ?>">appointments</a>
+        <a href="<?php echo URL . 'visits/index' ?>">visits</a>
+        <a href="<?php echo URL . 'admins/index' ?>">admins</a>
+        <a href="<?php echo URL . 'statistics/index' ?>">stats</a>
+        <a href="<?php echo URL . 'admins/logout' ?>">logout</a>
+        <?php
+    }
+    else if(isset($_SESSION['admin_id']) && $_SESSION['rank'] == 1)
+    {
+        ?>
+        <a href="<?php echo URL ?>">home</a>
+        <a href="<?php echo URL . 'inmates/index' ?>">inmates</a>
+        <a href="<?php echo URL . 'appointments/index' ?>">appointments</a>
+        <a href="<?php echo URL . 'visits/index' ?>">visits</a>
+        <a href="<?php echo URL . 'admins/index' ?>">admins</a>
+        <a href="<?php echo URL . 'statistics/index' ?>">stats</a>
+        <a href="<?php echo URL . 'admins/logout' ?>">logout</a>
+        <?php
+    }
+    else if(isset($_SESSION['admin_id']) && $_SESSION['rank'] == 2)
+    {
+        ?>
+        <a href="<?php echo URL ?>">home</a>
+        <a href="<?php echo URL . 'appointments/index' ?>">appointments</a>
+        <a href="<?php echo URL . 'admins/logout' ?>">logout</a>
+        <?php
+    }
+        ?>
+
+    <span><?php e($_SESSION['username'] ?? null); ?></span>
+    <?php
+        $rank = $_SESSION['rank'] ?? null;
+        if (!is_null($rank)) {
+            $ranks = ['super admin', 'admin', 'guard'];
+            ?>
+            <span>(<?php echo $ranks[$rank]; ?>)</span>
+            <?php
         }
-		
-		if (isset($_SESSION['user_id']))
-		{?>
-			<a href="<?php echo URL ?>">home</a>
-			
-			<a href="<?php echo URL . 'appointments/index' ?>">appointments</a>
-			<a href="<?php echo URL . 'visits/index' ?>">visits</a>
-			<a href="<?php echo URL . 'visitors/editaccount' ?>">profile </a>
-			<a href="<?php echo URL . 'visitors/logout' ?>">logout</a>
-			
-			<?php
-		}
-		 else if(isset($_SESSION['admin_id']) && $_SESSION['rank'] == 0) 
-				{
-					?>
-					<a href="<?php echo URL ?>">home</a>
-					<a href="<?php echo URL . 'inmates/index' ?>">inmates</a>
-					<a href="<?php echo URL . 'appointments/index' ?>">appointments</a>
-					<a href="<?php echo URL . 'visits/index' ?>">visits</a>
-					<a href="<?php echo URL . 'admins/index' ?>">admins</a>
-					<a href="<?php echo URL . 'statistics/index' ?>">stats</a>
-					<a href="<?php echo URL . 'visitors/logout' ?>">logout</a>
-					<?php
-				}
-		else if(isset($_SESSION['admin_id']) && $_SESSION['rank'] == 1) 
-				{?>
-					<a href="<?php echo URL ?>">home</a>
-					<a href="<?php echo URL . 'inmates/index' ?>">inmates</a>
-					<a href="<?php echo URL . 'appointments/index' ?>">appointments</a>
-					<a href="<?php echo URL . 'visits/index' ?>">visits</a>
-					<a href="<?php echo URL . 'admins/index' ?>">admins</a>
-					<a href="<?php echo URL . 'statistics/index' ?>">stats</a>
-					<a href="<?php echo URL . 'visitors/logout' ?>">logout</a>
-					<?php
-				}
-		else if(isset($_SESSION['admin_id']) && $_SESSION['rank'] == 2) 
-				{?>
-					<a href="<?php echo URL ?>">home</a>
-					<a href="<?php echo URL . 'appointments/index' ?>">appointments</a>			
-					<a href="<?php echo URL . 'visitors/logout' ?>">logout</a>
-					<?php
-				}				
-			
-				echo $_SESSION['username']??null;
-				echo " ";
-				echo $_SESSION['rank']??null;
-?>
-  
+    ?>
 </div>
