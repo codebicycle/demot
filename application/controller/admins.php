a0d58f519aedf976aab05eb687a34519a92f67b4
codebicycle <codebicycle@gmail.com>
2016-05-06 10:12:49 +0300

    Move logout to controller

--- application/controller/admins.php
+++ application/controller/admins.php
@@ -1,47 +1,42 @@
 <?php
 
-class admins extends Controller {
+class Admins extends Controller {
 
- 
- 
-  public function login() 
-  {
-    require APP . 'view/_templates/header.php';
-    require APP . 'view/admins/login.php';
-    require APP . 'view/_templates/footer.php';
-  }
-  
-  
+    public function login() 
+    {
+        require APP . 'view/_templates/header.php';
+        require APP . 'view/admins/login.php';
+        require APP . 'view/_templates/footer.php';
+    }
+
     public function index() {	
-	
-    require APP . 'view/_templates/header.php';
-    require APP . 'view/admins/index.php';
-    require APP . 'view/_templates/footer.php';
-  }
-  
-  public function addguard() {
-    require APP . 'view/_templates/header.php';
-    require APP . 'view/admins/addguard.php';
-    require APP . 'view/_templates/footer.php';
-  }
-  public function addadmin() {
-    require APP . 'view/_templates/header.php';
-    require APP . 'view/admins/addadmin.php';
-    require APP . 'view/_templates/footer.php';
-  }
- 
-  public function deleteadmin() {
-    require APP . 'view/_templates/header.php';
-    require APP . 'view/admins/deleteadmin.php';
-    require APP . 'view/_templates/footer.php';
-  }
-  public function logout() {	
-	
-    require APP . 'view/_templates/header.php';
-    require APP . 'view/admins/logout.php';
-    require APP . 'view/_templates/footer.php';
-  }
- 
- }
 
-?>
+        require APP . 'view/_templates/header.php';
+        require APP . 'view/admins/index.php';
+        require APP . 'view/_templates/footer.php';
+    }
+
+    public function addguard() {
+        require APP . 'view/_templates/header.php';
+        require APP . 'view/admins/addguard.php';
+        require APP . 'view/_templates/footer.php';
+    }
+    public function addadmin() {
+        require APP . 'view/_templates/header.php';
+        require APP . 'view/admins/addadmin.php';
+        require APP . 'view/_templates/footer.php';
+    }
+
+    public function deleteadmin() {
+        require APP . 'view/_templates/header.php';
+        require APP . 'view/admins/deleteadmin.php';
+        require APP . 'view/_templates/footer.php';
+    }
+
+    public function logout() {
+        session_start();
+        session_destroy();
+        header('location: ' . URL . 'admins/index');
+        die();
+    }
+}
--- application/controller/visitors.php
+++ application/controller/visitors.php
@@ -1,43 +1,42 @@
 <?php
 
-class visitors extends Controller {
+class Visitors extends Controller {
 
- 
-  public function login() {
-    require APP . 'view/_templates/header.php';
-    require APP . 'view/visitors/login.php';
-    require APP . 'view/_templates/footer.php';
-  }
-  
-  public function index() {
-    require APP . 'view/_templates/header.php';
-    require APP . 'view/visitors/index.php';
-    require APP . 'view/_templates/footer.php';
-  }
-  
-  public function register() {
-    require APP . 'view/_templates/header.php';
-    require APP . 'view/visitors/register.php';
-    require APP . 'view/_templates/footer.php';
-  }
- 
-  public function appointments() {
-    require APP . 'view/_templates/header.php';
-    require APP . 'view/visitors/appointments.php';
-    require APP . 'view/_templates/footer.php';
-  }
-  public function logout() {
-    require APP . 'view/_templates/header.php';
-    require APP . 'view/visitors/logout.php';
-    require APP . 'view/_templates/footer.php';
-  }
- 
-  public function editaccount() {
-    require APP . 'view/_templates/header.php';
-    require APP . 'view/visitors/editaccount.php';
-    require APP . 'view/_templates/footer.php';
-  }
- 
- }
+    public function login() {
+        require APP . 'view/_templates/header.php';
+        require APP . 'view/visitors/login.php';
+        require APP . 'view/_templates/footer.php';
+    }
 
-?>
+    public function index() {
+        require APP . 'view/_templates/header.php';
+        require APP . 'view/visitors/index.php';
+        require APP . 'view/_templates/footer.php';
+    }
+
+    public function register() {
+        require APP . 'view/_templates/header.php';
+        require APP . 'view/visitors/register.php';
+        require APP . 'view/_templates/footer.php';
+    }
+
+    public function appointments() {
+        require APP . 'view/_templates/header.php';
+        require APP . 'view/visitors/appointments.php';
+        require APP . 'view/_templates/footer.php';
+    }
+
+    public function editaccount() {
+        require APP . 'view/_templates/header.php';
+        require APP . 'view/visitors/editaccount.php';
+        require APP . 'view/_templates/footer.php';
+    }
+
+    public function logout() {
+        session_start();
+        session_destroy();
+        header('location: ' . URL . 'visitors/index');
+        die();
+    }
+
+}
